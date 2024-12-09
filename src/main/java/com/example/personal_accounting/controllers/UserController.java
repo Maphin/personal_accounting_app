8626f2c0e67c93bef1d97452693baedcd5f567f0
david.pozhar@gmail.com <david.pozhar@gmail.com>
2024-12-09 15:54:08 +0100

    added user contoller, dto and mapper

---
+++ src/main/java/com/example/personal_accounting/controllers/UserController.java
@@ -0,0 +1,25 @@
+package com.example.personal_accounting.controllers;
+
+import com.example.personal_accounting.configs.JwtRequestFilter;
+import com.example.personal_accounting.dto.User.UserDto;
+import com.example.personal_accounting.services.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(path = "/users")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+
+    @GetMapping("/my-profile")
+    public ResponseEntity<UserDto> getMe(@AuthenticationPrincipal JwtRequestFilter.CustomUserPrincipal principal) {
+        UserDto user = new UserDto();
+        user.setId(principal.userId());
+        user.setUsername(principal.username());
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+}
---
+++ src/main/java/com/example/personal_accounting/dto/User/UserDto.java
@@ -0,0 +1,8 @@
+package com.example.personal_accounting.dto.User;
+import lombok.Data;
+
+@Data
+public class UserDto {
+    private Long id;
+    private String username;
+}
